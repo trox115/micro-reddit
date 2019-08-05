501c5bf1d5ad710eba48b05a3f3c3da4b527f868
trox115 <tofernandes67@gmail.com>
2019-08-05 19:10:48 +0100

    created users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true
+
+    validates :email, presence: true, length: {minimum:4, maximum:255},
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness:  { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 8 }
+    has_secure_password
+    has_many :posts
+    has_many :comments
+
+
+
+
 end
