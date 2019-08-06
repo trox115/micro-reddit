ef055ce7f41d8845424de02dae600e5eab239b85
trox115 <tofernandes67@gmail.com>
2019-08-06 19:27:57 +0100

    added validations to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: {maximum:255}
+
+  validates :body, presence: true, length: {minimum:10}
   has_many :comments
 end
