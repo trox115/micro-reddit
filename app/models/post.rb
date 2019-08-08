8ce39d0aaa018a2c0a821bf0bbc98b15453aa561
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-08 12:19:35 -0500

    Posts Validation Finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {minimum:1, maximum:60}
+  validates :body, presence: true, length: {minimum:1}
 end
